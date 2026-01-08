7de625ff35c8516e2f9325cfd8ad8092ff53e670
Lily <haquynhchi@gmail.com>
2026-01-08 20:33:59 +1100

    Recreated profile delete trigger

---
+++ prisma/migrations/20260108092945_recreated_profile_delete_trigger/migration.sql
@@ -0,0 +1,38 @@
+CREATE OR REPLACE FUNCTION handle_profile_delete()
+    RETURNS TRIGGER
+    AS $$
+BEGIN
+    DELETE FROM foods
+    WHERE owner_user_id = OLD.id
+        AND is_public = FALSE;
+
+    UPDATE foods
+    SET
+        owner_user_id = NULL,
+        deleted_at = now()
+    WHERE
+        owner_user_id = OLD.id
+        AND is_public = TRUE;
+
+    DELETE FROM creations
+    WHERE owner_user_id = OLD.id
+        AND is_public = FALSE;
+
+    UPDATE creations
+    SET
+        owner_user_id = NULL,
+        deleted_at = now()
+    WHERE
+        owner_user_id = OLD.id
+        AND is_public = TRUE;
+        
+    RETURN old;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER on_profile_delete
+    AFTER DELETE ON profiles
+    FOR EACH ROW
+    EXECUTE FUNCTION handle_profile_delete();
+
