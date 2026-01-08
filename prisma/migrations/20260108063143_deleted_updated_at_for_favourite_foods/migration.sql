e86f1137508ec4cb78f8bb0b35532f8381b3dcf5
Lily <haquynhchi@gmail.com>
2026-01-08 17:43:31 +1100

    Deleted updated_at for favourite_foods

---
+++ prisma/migrations/20260108063143_deleted_updated_at_for_favourite_foods/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `updated_at` on the `favourite_foods` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "favourite_foods" DROP COLUMN "updated_at";
