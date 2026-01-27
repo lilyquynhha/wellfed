da18596c83086ab753e82bd5e00620e9b0f87938
Lily <haquynhchi@gmail.com>
2026-01-27 15:37:54 +1100

    refactor: fetch selected food from SearchResults component

--- components/foods/search-results.tsx
+++ components/foods/search-results.tsx
@@ -2,9 +2,11 @@
 
 import FoodsList from "./foods-list";
 import FoodPreview from "./food-preview";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MAX_RESULTS } from "@/lib/actions/food/food-crud";
-import { spFood } from "@/lib/supabase/database-types";
+import { spFood, spServing } from "@/lib/supabase/database-types";
+import { createClient } from "@/lib/supabase/client";
+import { PopupPreviewSkeleton, NormalPreviewSkeleton } from "../ui/skeleton";
 
 export default function SearchResults({
   foods,
@@ -15,18 +17,47 @@ export default function SearchResults({
   totalResults: number;
   page: number;
 }) {
+  const supabase = createClient();
   if (totalResults == 0) return <p>No food found.</p>;
 
   const [currentFoods, setCurrentFoods] = useState(foods);
-  const [selectedFood, setSelectedFood] = useState<spFood | null>(null);
+  const [selectedFood, setSelectedFood] = useState<spFood>(foods[0]);
+  const [selectedServings, setSelectedServings] = useState<spServing[]>([]);
+  const [isPending, setIsPending] = useState(false);
 
   const totalPages = Math.ceil(totalResults / MAX_RESULTS);
 
+  // Fetch selected food
+  useEffect(() => {
+    setSelectedServings([]);
+
+    async function getServings() {
+      setIsPending(true);
+      const { data, error } = await supabase
+        .from("servings")
+        .select()
+        .eq("owner_food_id", selectedFood.id);
+
+      setIsPending(false);
+      console.log("done!");
+
+      if (error || !data) {
+        return [];
+      }
+
+      return data as spServing[];
+    }
+
+    getServings().then(setSelectedServings);
+  }, [selectedFood]);
+
   // Reset selected food if it is a new food search
   if (foods != currentFoods) {
-    setSelectedFood(null);
     setCurrentFoods(foods);
+    setSelectedFood(foods[0]);
   }
+
+  const isMobile = typeof window !== "undefined" && window.innerWidth < 768;
 
   return (
     <>
@@ -37,7 +68,16 @@ export default function SearchResults({
           totalPages={totalPages}
           onSelect={setSelectedFood}
         />
-        <FoodPreview food={selectedFood} />
+
+        {isPending || selectedServings.length == 0 ? (
+          isMobile ? (
+            <PopupPreviewSkeleton />
+          ) : (
+            <NormalPreviewSkeleton />
+          )
+        ) : (
+          <FoodPreview food={selectedFood} servings={selectedServings} />
+        )}
       </div>
     </>
   );
